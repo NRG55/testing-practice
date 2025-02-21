f9595d63fc6b91ca3bf0f69eef36c71c3ad9fda4
NRG55 <maloj_1980@yahoo.fr>
2025-02-21 02:14:06 +0000

    Add: a reverseString test function and a reverseString function

---
+++ functions/reverseString.js
@@ -0,0 +1,5 @@
+export default function reverseString(string) {
+    const reversedString = string.split('').reverse().join('');
+
+    return reversedString;
+};
---
+++ tests/reverseString.test.js
@@ -0,0 +1,5 @@
+import reverseString from "../functions/reverseString.js";
+
+test('reverses a string', () => {
+    expect(reverseString('string')).toBe('gnirts');
+});
