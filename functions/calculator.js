412f49a243310c2d6da9b201b8d0b1ca94330ed6
NRG55 <maloj_1980@yahoo.fr>
2025-02-24 00:53:49 +0000

    Add(calculator.test.js): a throw error test (division by zero). Update(calculator.js): divide property.

--- functions/calculator.js
+++ functions/calculator.js
@@ -6,6 +6,10 @@ export const calculator = {
         return a - b;
     },
     divide: function (a, b) {
+        if (b === 0) {
+           throw new Error("Can not divide by zero");
+        };
+
         return a / b;
     },
     multiply: function (a, b) {
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -13,6 +13,10 @@ describe("calculator", () => {
         expect(calculator.divide(8, 4)).toBe(2);
     });
 
+    test("divide by zero should throw an error", () => {
+        expect(() => calculator.divide(8, 0)).toThrow("Can not divide by zero");
+    });
+
     test("multiply two numbers", () => {
         expect(calculator.multiply(2, 3)).toBe(6);
     });
